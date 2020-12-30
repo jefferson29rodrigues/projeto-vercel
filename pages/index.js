47a356f855f0318d6a9f6f596afaf79f07c00eb8
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-12-30 14:39:34 -0300

    novo nome

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import {useState} from 'react';
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Sejam Bem Vindos</h1>
+            <h1>Site do <span style="color: red;">Jeff</span> e da <span style="color: blue;">Juh</span></h1>
+            <h2>Hospedado na vercel - <strong>dica do Filipe Deschamps</strong></h2>
             <Contador />
         </div>
     )
